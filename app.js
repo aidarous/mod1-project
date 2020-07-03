d8bd16bea578f2c41ce665d733e45c3cd74ed68f
Student_MA102 <aidarous12@gmail.com>
2020-07-03 10:26:16 -0400

    Revised archetype and moveset code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Basketball game rules and mechanics 
 * Get to the basket by winning 3 turns before time runs out
-* Guard moves are: shoot, pass, and steal
+* Guard moves are: shoot, assist, and steal
 * Forward moves are: catch and shoot, cut to basket, and help defense
 * Center moves are: dunk, pick and roll, block
 *
@@ -10,12 +10,10 @@
 */
 
 
-/* Player class and archetype */
+/* Archetype and moveset */
 
-// all archetype stats will be on a scale of 1-10
-class Player  {
-    constructor(name, firstMove, secondMove, thirdMove){
-        this.name = name;
+class Moveset {
+    constructor(){
         this.firstMove = firstMove;
         this.secondMove = secondMove;
         this.thirdMove = thirdMove;
@@ -23,20 +21,19 @@ class Player  {
 }
 
 
-let choose =prompt("Please pick a player \nPress 1 for Guard \nPress 2 for Forward \nPress 3 for Center")
 
-if(choose == "1"){
-    let guard = new Player("Guard", "Shoot", "Pass", "Steal")
-    console.log("You choose Guard!")
-    console.log(guard)
-}else if(choose == "2"){
-    let forward = new Player("Forward", "Catch and shoot", "Cut to basket", "Help defense")
-    console.log("You choose Forward")
-}else if(choose == "3"){
-    let center = new Player("Center", "Dunk", "Pick and roll", "Block")
-    console.log("You choose Center")
-}
-    
+
+let shotClock = 24;
+//let choose = prompt("Please pick a player \nPress 1 for Guard \nPress 2 for Forward \nPress 3 for Center")
+let players = ["Guard", "Forward", "Center"]
+
+
+let chooseOpponent = Math.floor(Math.random()*3);
+let opponent = players[chooseOpponent]
+console.log(opponent)
+// create conditons for players to score
+// 
+
     
     
 
