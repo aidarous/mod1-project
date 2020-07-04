c9e6df12067db258bbd83d7408a0362dca444eff
Student_MA102 <aidarous12@gmail.com>
2020-07-04 12:35:52 -0400

    Swapped original project goal for a simpler one and setup webpage elements

--- app.js
+++ app.js
@@ -1,45 +1,20 @@
-/* Basketball game rules and mechanics 
-* Get to the basket by winning 3 turns before time runs out
-* Guard moves are: shoot, assist, and steal
-* Forward moves are: catch and shoot, cut to basket, and help defense
-* Center moves are: dunk, pick and roll, block
-*
-* Shoot/Catch and shoot/dunk beats: pass, cut to basket, and pick and roll
-* Pass/Cut to basket/Pick and roll beats: Steal, Help Defense, and block
-* Steal/ Help Defense/ Block beats: Shoot, Catch and Shoot, and dunk 
-*/
+/* Basketball RPS */
 
 
-/* Archetype and moveset */
-
-class Moveset {
-    constructor(firstMove, secondMove, thirdMove){
-        this.firstMove = firstMove;
-        this.secondMove = secondMove;
-        this.thirdMove = thirdMove;
-    }
-}
+/* Moveset */
 
 
 
-let shotClock = 24;
+// const shotClock = 24;
+const playerScore = 0;
+const opponentScore = 0;
 
-let players = ["Guard", "Forward", "Center"]
-let guardMoveset = new Moveset("Shoot", "Pass", "Steal") 
-let forwardMoveset = new Moveset("Catch and shoot", "Cut to basket", "Help defense")
-let centerMoveset = new Moveset("Dunk", "Pick and roll", "Block")
-let chooseOpponent = Math.floor(Math.random()*3);
+let moveSet = ["Shoot", "Pass", "Steal"] 
+
+let choiceGenerator = Math.floor(Math.random()*3); // randomizes the choice made by the computer
 let opponent = players[chooseOpponent]
 console.log(opponent)
 
-// choose a player
-let choosePlayer = () => {
-
-
-}
-
-// create conditons for players to score
-// 
 
     
     
