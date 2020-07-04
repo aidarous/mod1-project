bbf575e59aefc5831f3145759ccd16eaec71c64a
Student_MA102 <aidarous12@gmail.com>
2020-07-04 15:52:50 -0400

    Troubleshot click functionality for future use

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 /* Basketball RPS */
 
 
-/* Moveset */
-
-
+/* Set up Global variables for future use*/
 
 // const shotClock = 24;
 const playerScore = 0;
 const opponentScore = 0;
 
-let moveSet = ["Shoot", "Pass", "Steal"] 
-
+let moveSet = ["Shoot", "Pass", "Block"] 
 let choiceGenerator = Math.floor(Math.random()*3); // randomizes the choice made by the computer
-let opponent = players[chooseOpponent]
-console.log(opponent)
+
+    let playerShoot = () => {
+        document.getElementById('playerClick').innerHTML = 'Shoot!'
+    }
+    let playerPass = () => {
+        document.getElementById('playerClick').innerHTML = 'Pass!'
+    }
+    let playerBlock = () => {
+        document.getElementById('playerClick').innerHTML = 'Block!'
+    }
+
+
+
+
+
+
+
 
 
     
