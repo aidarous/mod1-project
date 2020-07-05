1624dfcfc417377b4724af0c87d14cc9974f5e00
Student_MA102 <aidarous12@gmail.com>
2020-07-05 13:30:54 -0400

    Score conditions set

--- app.js
+++ app.js
@@ -4,39 +4,29 @@
 /* Set up Global variables for future use*/
 
 // const shotClock = 24;
-const playerScore = 0;
-const opponentScore = 0;
+let playerScore = 0;
+// let playerElementScore = document.getElementById('player-score');
+let opponentScore = 0;
+// let opponentElementScore = document.getElementById('opponent-score');
 
 let moveSet = ["Shoot", "Pass", "Block"] 
-let choiceGenerator = Math.floor(Math.random()*3); // randomizes the choice made by the computer
-let opponentMove = moveSet[choiceGenerator];
-
-let playerShoot = () => {   
-    document.getElementById('playerClick').innerHTML = 'You chose shoot!'   
-    document.getElementById('opponentChoice').innerHTML = `Opponent chose ${opponentMove}!`
-    }
-     
-let playerPass = () => {  
-    document.getElementById('playerClick').innerHTML = 'You chose pass!'
-    document.getElementById('opponentChoice').innerHTML = `Opponent chose ${opponentMove}!`
-    }  
-
-let playerBlock = () => {
-    document.getElementById('playerClick').innerHTML = 'You chose block!'
-    document.getElementById('opponentChoice').innerHTML = `Opponent chose ${opponentMove}!`
-    }
+let choiceGenerator = Math.floor(Math.random()*3);
+let opponentChoice = moveSet[choiceGenerator];
+// let player = {
+//     firstChoice: "Shoot",
+//     secondChoice: "Pass",
+//     thirdChoice: "Block"
+// }
 
 
-
-
-
-
-
-
-
-    
-    
-
-
+//Compare player decision and oppenent decision
+//win conditions
+function game(playerChoice){
+    if(playerChoice == "Shoot" && opponentChoice == "Pass" || playerChoice == "Pass" && opponentChoice == "Block" || playerChoice == "Block" && opponentChoice == "Shoot"){
+        console.log("Win!")
+        document.getElementById('playerClick').innerText = `You Win! You chose ${playerChoice} and opponent chose ${opponentChoice}`
+        playerScore++;
+    } else if()
+}
 
 
