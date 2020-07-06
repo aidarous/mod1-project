942733a7ac7db8dac7a5e312e781fb5b81281fc5
Student_MA102 <aidarous12@gmail.com>
2020-07-06 00:02:05 -0400

    Fixed opponent choice to randomize every turn

--- app.js
+++ app.js
@@ -16,38 +16,56 @@ let opponentChoice = moveSet[choiceGenerator];
 
 
 let gameOver = () => {
-    
+    /* Resets game player wins or loses */
     if(playerScore == 16){
         alert("YOU WIN!!")
-        let = opponentScore = 0;
-        let = playerScore = 0;
+        opponentScore = 0;
+        displayOpponentScore.innerHTML = `${opponentScore}`
+        playerScore = 0;
+        displayPlayerScore.innerHTML = `${playerScore}`
+        document.getElementById('playerClick').innerHTML = ""
     } else if(opponentScore == 16){
         alert("YOU LOSE")
-        let = opponentScore = 0;
-        let = playerScore = 0;
+        opponentScore = 0;
+        displayOpponentScore.innerHTML = `${opponentScore}`
+        playerScore = 0;
+        displayPlayerScore.innerHTML = `${playerScore}`
+        document.getElementById('playerClick').innerHTML = ""
     }
 
 }
 
+let restartGame = () =>{
 
+}
+// 
+let resetOpponentDecision = () => {
+    choiceGenerator = Math.floor(Math.random()*3);
+    opponentChoice = moveSet[choiceGenerator];
+}
 
 
 function game(playerChoice){
     if(playerScore <= 16 || opponentScore <= 16){ 
         if(playerChoice === "Shoot" && opponentChoice === "Pass" || playerChoice === "Pass" && opponentChoice === "Block" || playerChoice === "Block" && opponentChoice === "Shoot"){// conditions for player to score
-            console.log("Win!")
             let win = document.getElementById('playerClick')
             win.innerText = `Well done! You chose ${playerChoice} and opponent chose ${opponentChoice}`
             playerScore++;
             displayPlayerScore.innerHTML = `${playerScore}`
+            resetOpponentDecision()
+            console.log(opponentChoice)
         } else if(opponentChoice === "Shoot" && playerChoice === "Pass" || opponentChoice === "Pass" && playerChoice === "Block" || opponentChoice === "Block" && playerChoice === "Shoot"){ // conditions for opponent to score
             let lose = document.getElementById('playerClick')
             lose.innerText = `Touch luck! You chose ${playerChoice} and opponent chose ${opponentChoice}`
             opponentScore++;
             displayOpponentScore.innerHTML = `${opponentScore}`
+            resetOpponentDecision()
+            console.log(opponentChoice)
         } else{
             let draw = document.getElementById('playerClick')
             draw.innerText = `You chose ${playerChoice} and your opponent chose ${opponentChoice}! Its a draw!`
+            resetOpponentDecision()
+            console.log(opponentChoice)
         }
         gameOver()
     }
