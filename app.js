6c35a13d3b4cb2787e9c2580fcad374855879568
Student_MA102 <aidarous12@gmail.com>
2020-07-06 22:51:03 -0400

    Game summary modal complete

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 /* Basketball RPS */
 
-//modal containing game instructions
-let modal = document.getElementById('modal-container');
-let btn = document.getElementById('modal-button');
+// Get the modal
+var modal = document.getElementById("modal-container");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modal-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
-let span = document.getElementById('close')[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 /* Set up Global variables and functions for future use*/
 
 let playerScore = 0;
