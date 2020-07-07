f94754f530c478ab8e465ef0c32881e68746ae20
Student_MA102 <aidarous12@gmail.com>
2020-07-06 23:15:33 -0400

    modifed css

--- app.js
+++ app.js
@@ -2,24 +2,18 @@
 
 // Get the modal
 var modal = document.getElementById("modal-container");
-
-// Get the button that opens the modal
 var btn = document.getElementById("modal-button");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+// opens modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
+// closes the modal from the x
 span.onclick = function() {
   modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
+// closes modal from anywhere on screen
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
